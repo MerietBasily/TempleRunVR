1c2c7919ad0eda112bce2eb9640cd6b88f86278d
Eric Dittus <ericdittus@Erics-Air.hitronhub.home>
2022-06-26 15:34:49 -0400

    Added jump and slide to alien, and made the wolf character aswell with a simple attack sequence to be improved

--- Assets/AnimationStateController.cs
+++ Assets/AnimationStateController.cs
@@ -9,41 +9,74 @@ public class AnimationStateController : MonoBehaviour
     float blendX = 0.0f;
     public float acceleration = 2.0f;
     public float deceleration =2.0f;
-    public float maximumWalkVelocity = 0.5f;
+    //public float maximumWalkVelocity = 0.5f;
     public float maximumRunVelocity = 2.0f;
     // Start is called before the first frame update
+    public bool obstacleHit;
     void Start()
     {
         animator = GetComponent<Animator>();
-    }
+        obstacleHit = animator.GetBool("obstacleHit");
 
+    }
+    
     // Update is called once per frame
     void Update()
     {
         bool forwardPress = Input.GetKey("w");
         bool leftPress = Input.GetKey("a");
         bool rightPress = Input.GetKey("d");
+        bool slidePress = Input.GetKey("s");
+        bool isSlide = animator.GetBool("isSlide");
+        bool jumpPress = Input.GetKey("space");
+        bool isJump = animator.GetBool("isJump");
         bool runPress = Input.GetKey("left shift");
+        bool hit = Input.GetKey("k");
 
-        float currentMaxVelocity = runPress ? maximumRunVelocity : maximumWalkVelocity;
-        if(forwardPress && (blendZ < currentMaxVelocity) ){
+        if(hit){
+            animator.SetBool("obstacleHit",true);
+        }else if(!hit){
+            animator.SetBool("obstacleHit",false);
+        }
+        
+
+        float currentMaxVelocity = maximumRunVelocity;
+        // start walking to running
+
+        if(blendZ < currentMaxVelocity ){
             blendZ += Time.deltaTime * acceleration;
         }
+        // turn left
         if(leftPress && (blendX > -currentMaxVelocity) ){
             blendX -= Time.deltaTime * acceleration;
         }
+        // turn right
         if(rightPress && (blendX < currentMaxVelocity) ){
             blendX += Time.deltaTime * acceleration;
         }
-
-        //Deceleration forward
-        if(!forwardPress && (blendZ > 0.0f)){
-            blendZ -= Time.deltaTime * deceleration;
+        // jump
+        if(animator.GetBool("isJump") == true){
+            animator.SetBool("isJump",false);
+            
         }
-        if(!forwardPress && (blendZ <0.0f)){
-          blendZ = 0.0f;  
+        if(jumpPress){
+                animator.SetBool("isJump",false);
+            }
+        if(jumpPress && animator.GetBool("isJump") == false){
+            animator.SetBool("isJump",true);
+            jumpPress =  false;
+            //animator.SetBool("isJump",false);
         }
 
+        // slide
+        if(animator.GetBool("isSlide") == true){
+            animator.SetBool("isSlide",false);
+        }
+        if(slidePress && animator.GetBool("isSlide") == false){
+            animator.SetBool("isSlide",true);
+            slidePress = false;
+            //animator.SetBool("isSlide",false);
+        }
         //Deceleration left/right
         if(!leftPress && (blendX < 0.0f)){
             blendX += Time.deltaTime * deceleration;
@@ -58,39 +91,36 @@ public class AnimationStateController : MonoBehaviour
         }
 
         // lock forward, else decelerate to the maximum walk velocity
-        if(forwardPress && runPress && blendZ > currentMaxVelocity){
+        if(blendZ > currentMaxVelocity){
             blendZ = currentMaxVelocity;
-        }else if(forwardPress && blendZ > currentMaxVelocity){
+        }
+        if(obstacleHit && obstacleHit && blendZ > currentMaxVelocity){
             blendZ -= Time.deltaTime * deceleration;
             if(blendZ > currentMaxVelocity && blendZ < (currentMaxVelocity - 0.05f)){
                 blendZ = currentMaxVelocity;
             }
-        }else if(forwardPress && blendZ < currentMaxVelocity && blendZ > (currentMaxVelocity - 0.05f)){
-            blendZ = currentMaxVelocity;
         }
 
         // lock left, else decelerate to the maximum walk velocity
-        if(leftPress && runPress && blendZ < -currentMaxVelocity){
+        if(leftPress && blendZ < -currentMaxVelocity){
             blendZ = -currentMaxVelocity;
-        }else if(leftPress && blendZ < -currentMaxVelocity){
+        }
+        if(leftPress && obstacleHit && blendZ < -currentMaxVelocity){
             blendZ -= Time.deltaTime * deceleration;
             if(blendZ < -currentMaxVelocity && blendZ > (-currentMaxVelocity - 0.05f)){
                 blendZ = -currentMaxVelocity;
             }
-        }else if(leftPress && blendZ > currentMaxVelocity && blendZ < (-currentMaxVelocity + 0.05f)){
-            blendZ = -currentMaxVelocity;
         }
 
         // lock right, else decelerate to the maximum walk velocity
-        if(rightPress && runPress && blendZ > currentMaxVelocity){
+        if(rightPress && blendZ > currentMaxVelocity){
             blendZ = currentMaxVelocity;
-        }else if(rightPress && blendZ > currentMaxVelocity){
+        }
+        if(rightPress && obstacleHit && blendZ > currentMaxVelocity){
             blendZ -= Time.deltaTime * deceleration;
             if(blendZ > currentMaxVelocity && blendZ < (currentMaxVelocity - 0.05f)){
                 blendZ = currentMaxVelocity;
             }
-        }else if(rightPress && blendZ < currentMaxVelocity && blendZ > (-currentMaxVelocity - 0.05f)){
-            blendZ = currentMaxVelocity;
         }
 
         animator.SetFloat("BlendZ",blendZ);
